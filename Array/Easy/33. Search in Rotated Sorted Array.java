5af7348875db6b8224bdf4268cebcbafc9cc2604
danny boi <43886538+danushv@users.noreply.github.com>
2024-04-27 14:59:53 -0500

    Create 33. Search in Rotated Sorted Array.java

---
+++ Array/Easy/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        // first step is to find the smallest element
+
+        int left=0;
+        int right=nums.length-1;
+       
+        while(left<right)
+        {
+             int midpoint=left+(right-left)/2;
+            if(nums[midpoint]>nums[right])
+            {
+                left=midpoint+1;
+            }
+            else
+            {
+                right=midpoint;
+            }
+        }
+
+        int start=left;
+        left=0;
+        right=nums.length-1;
+
+        if(target>=nums[start]&&target<=nums[right])
+        {
+            left=start;
+        }
+        else
+        {
+            right=start;
+        }
+
+
+
+
+        while(left<=right)
+        {
+             int midpoint=left+(right-left)/2;
+             if(nums[midpoint]==target)
+             return midpoint;
+             else if(nums[midpoint]>target)
+             {
+                right=midpoint-1;
+             }
+             else
+             {
+                left=midpoint+1;
+             }
+        }
+
+        return -1;
+    }
+}

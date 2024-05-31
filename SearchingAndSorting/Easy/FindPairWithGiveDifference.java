29ca72d4cc56a8f23374faab76a1b39cd719d585
danny boi <43886538+danushv@users.noreply.github.com>
2024-05-31 13:28:02 -0500

    31-May

---
+++ SearchingAndSorting/Easy/FindPairWithGiveDifference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findPair(int n, int x, int[] arr) {
+        // code here
+        
+        Arrays.sort(arr);
+        
+        int i=0;
+        int j=1;
+        while(i<n&&j<n)
+        {
+            if(i!=j&&arr[j]-arr[i]==x)
+            return 1;
+            else if(arr[j]-arr[i]>x)
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
